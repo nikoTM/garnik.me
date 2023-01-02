c517231099e26dfff289fff76e22f08e52d725fe
nikotm <nikotm@gmail.com>
2023-01-02 17:31:18 +0000

    switch to astro

--- src/components/description-text.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const DescriptionText = styled.p`
-  color: #ffffff;
-  max-width: 305px;
-`;
-
-export default DescriptionText;
--- src/components/small-container.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const SmallContainer = styled.div`
-  margin: auto;
-  max-width: 350px;
-`;
-
-export default SmallContainer;
